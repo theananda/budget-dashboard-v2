8c18ab65a676616147ae38b32919128ecd97fcf0
DragonVirus <limonster.li@gmail.com>
2019-05-15 12:45:07 +0630

    change department button text

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -3,7 +3,7 @@
     <div class="mdl-typography--text-center">
       <h1>Budget Dashboard</h1>
       <router-link to="/regions" class="mdl-button mdl-js-button mdl-button--raised mdl-button--colored" style="margin:20px;">Explore by Regions</router-link>
-      <router-link to="/departments" class="mdl-button mdl-js-button mdl-button--raised mdl-button--colored" style="margin:20px;">Explore by Departments</router-link>  
+      <router-link to="/departments" class="mdl-button mdl-js-button mdl-button--raised mdl-button--colored" style="margin:20px;">Explore by Sectors</router-link>  
     </div>
   </div>
 </template>
