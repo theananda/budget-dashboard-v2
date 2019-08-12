ee6a3b4d4a2c9bcdf1a121c11d74d998af45c27e
DragonVirus <limonster.li@gmail.com>
2019-08-12 23:00:59 +0630

    Add v-select component

--- src/main.js
+++ src/main.js
@@ -3,6 +3,10 @@ import App from './App.vue'
 import 'leaflet/dist/leaflet.css'
 import './assets/css/style.css'
 import router from './router'
+import vSelect from 'vue-select'
+import 'vue-select/dist/vue-select.css';
+
+Vue.component('v-select', vSelect)
 
 Vue.config.productionTip = false
 
