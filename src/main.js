f14572f9c977a2f2464cc728ad11c0c494a57f26
DragonVirus <limonster.li@gmail.com>
2019-08-13 16:06:48 +0630

    Add vue-router-back-button package

--- src/main.js
+++ src/main.js
@@ -5,10 +5,14 @@ import './assets/css/style.css'
 import router from './router'
 import vSelect from 'vue-select'
 import 'vue-select/dist/vue-select.css';
+import { routerHistory, writeHistory } from 'vue-router-back-button'
 
 Vue.component('v-select', vSelect)
 
 Vue.config.productionTip = false
+
+Vue.use(routerHistory)
+router.afterEach(writeHistory)
 
 new Vue({
   render: h => h(App),
