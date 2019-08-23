6c2be214bde99370938a68daa1e2e7e8823dca6b
DragonVirus <limonster.li@gmail.com>
2019-08-23 16:54:34 +0630

    Remove unionTotal chart in chart config for region page

--- src/config/charts.js
+++ src/config/charts.js
@@ -34,26 +34,5 @@ export default {
 		},
 		series: [],
 		viz_desc: '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid excepturi minima sed? Nostrum tenetur libero eligendi. Autem harum culpa, eveniet laborum voluptatibus, amet porro officia, corporis soluta ab, dolor dolores.</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid excepturi minima sed? Nostrum tenetur libero eligendi. Autem harum culpa, eveniet laborum voluptatibus, amet porro officia, corporis soluta ab, dolor dolores.</p>'
-	},
-	unionTotal : {
-		title: 'Union Total Budget',
-		type: 'pie',
-		width: '380',
-		position: 'right',
-		series: [],
-		options: {
-		  labels: ['Team A', 'Team B', 'Team C', 'Team D', 'Team E'],
-		  responsive: [{
-			breakpoint: 480,
-			options: {
-			  chart: {
-				width: 200
-			  },
-			  legend: {
-				position: 'bottom'
-			  }
-			}
-		  }]
-		}
 	}
 }
