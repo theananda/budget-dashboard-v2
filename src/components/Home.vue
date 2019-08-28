db269ffbb7ca411802b808e813dc2f329dfcbff6
DragonVirus <limonster.li@gmail.com>
2019-08-28 15:43:04 +0630

    add logo to home page

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -6,6 +6,11 @@
   		  <p>Myanmar Budget Explorer is designed to provide a simple tool for exploring Myanmar's published budget data in the interest of enhancing transparency and encouraging discussion and analysis of Myanmar's budget process and allocations.</p>
   		  <router-link to="/regions" class="mdl-button mdl-js-button mdl-button--raised mdl-button--colored" style="margin:20px;">Explore by Regions</router-link>
   		  <router-link to="/2019/Union/sectors" class="mdl-button mdl-js-button mdl-button--raised mdl-button--colored" style="margin:20px;">Explore by Sectors</router-link>  
+        <div class="project-info">
+          <p>This project is developed by</p>
+          <p class="ananda_logo"><img src="../assets/images/logo.png" height="50" width="50" alt="The Ānanda">The Ānanda</p>
+          <p>and funded by <a href="https://asiafoundation.org/where-we-work/myanmar/" target="_blank">The Asia Foundation</a></p> 
+        </div>
   		</div>
   		<div class="photo-credit">
   			Photo by <a href="https://unsplash.com/@ray30?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText" target="_blank">ray rui</a> on <a href="https://unsplash.com/photos/QzmA1qSqTwA?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText" target="_blank">Unsplash</a>
