206700e07773884c8747a4a3690badf7c0107f57
DragonVirus <limonster.li@gmail.com>
2019-08-29 14:32:38 +0630

    change button colors in home page

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -4,8 +4,8 @@
   		<div class="mdl-typography--text-center intro-wrapper">
   		  <h1>Myanmar Budget Dashboard</h1>
   		  <p>Myanmar Budget Explorer is designed to provide a simple tool for exploring Myanmar's published budget data in the interest of enhancing transparency and encouraging discussion and analysis of Myanmar's budget process and allocations.</p>
-  		  <router-link to="/regions" class="mdl-button mdl-js-button mdl-button--raised mdl-button--colored" style="margin:20px;">Explore by Regions</router-link>
-  		  <router-link to="/2019/Union/sectors" class="mdl-button mdl-js-button mdl-button--raised mdl-button--colored" style="margin:20px;">Explore by Sectors</router-link>  
+  		  <router-link to="/regions" class="mdl-button mdl-js-button mdl-button--raised mdl-button--accent mdl-color-text--white" style="margin:20px;">Explore by Regions</router-link>
+  		  <router-link to="/2019/Union/sectors" class="mdl-button mdl-js-button mdl-button--raised mdl-button--accent mdl-color-text--white" style="margin:20px;">Explore by Sectors</router-link>  
         <div class="project-info">
           <p>This project is developed by</p>
           <p class="ananda_logo"><a href="https://theananda.org" target="_blank"><img src="../assets/images/logo.png" height="50" width="50" alt="The Ānanda">The Ānanda</a></p>
