53244dac63ae437cbda3ef577014482ec47d9b20
DragonVirus <limonster.li@gmail.com>
2019-09-02 15:02:06 +0630

    Add VueAnalytic Package

--- src/main.js
+++ src/main.js
@@ -6,6 +6,8 @@ import router from './router'
 import vSelect from 'vue-select'
 import 'vue-select/dist/vue-select.css';
 import { routerHistory, writeHistory } from 'vue-router-back-button'
+import VueAnalytics from 'vue-analytics'
+import config from './config/index.js'
 
 Vue.component('v-select', vSelect)
 
@@ -13,6 +15,10 @@ Vue.config.productionTip = false
 
 Vue.use(routerHistory)
 router.afterEach(writeHistory)
+
+Vue.use(VueAnalytics, {
+  id: config.google_analytic_id
+})
 
 new Vue({
   render: h => h(App),
