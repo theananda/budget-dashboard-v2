f18cd7f65a17b13255910d782725972c12c41b63
DragonVirus <limonster.li@gmail.com>
2020-01-29 16:45:17 +0630

    update data download link

--- src/components/partials/DataDownload.vue
+++ src/components/partials/DataDownload.vue
@@ -1,6 +1,6 @@
 <template>
 	<a
-		href="https://docs.google.com/spreadsheets/d/e/2PACX-1vRJiEngzQBzR-CpgE83dGJlJ6bdnQKtqcHsv5EPYkQ39JSyRFnRAWDvv7ShCdBoqNOwJE-P_L5fhYdh/pub?gid=1714950484&single=true&output=csv"
+		href="https://data.theananda.org/Budget/budget_data_20200129_0354.csv"
 		target="_blank"
 		class="mdl-button mdl-js-button data-download-btn">
 		<i class="material-icons">get_app</i> Download Data
