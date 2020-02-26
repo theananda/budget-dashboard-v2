61fcaf61122d301f667e8fbb0783069879c6bd6f
DragonVirus <limonster.li@gmail.com>
2020-02-26 17:37:33 +0630

    Update new download data link

--- src/components/partials/DataDownload.vue
+++ src/components/partials/DataDownload.vue
@@ -1,6 +1,6 @@
 <template>
 	<a
-		href="https://data.theananda.org/Budget/budget_data_20200129_0354.csv"
+		href="https://data.theananda.org/Budget/budget_data_20200226_1731.csv"
 		target="_blank"
 		class="mdl-button mdl-js-button data-download-btn">
 		<i class="material-icons">get_app</i> Download Data
